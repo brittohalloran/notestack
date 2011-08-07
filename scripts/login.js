ce570735360495be5b6173c25c9a70432674fa84
Britt O'Halloran <britt.j.ohalloran@gmail.com>
2011-08-07 10:00:14 -0500

    fix login issue

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,7 @@
+// JQUERY NO CONFLICT
+var $ = jQuery.noConflict();
+
+
 // GET URL VARS PLUGIN
 $.extend({getUrlVars:function(){for(var a=[],b,d=window.location.href.slice(window.location.href.indexOf("?")+1).split("&"),c=0;c<d.length;c++)b=d[c].split("="),a.push(b[0]),a[b[0]]=b[1];return a},getUrlVar:function(a){return $.getUrlVars()[a]}});
 
@@ -5,9 +9,6 @@ $.extend({getUrlVars:function(){for(var a=[],b,d=window.location.href.slice(wind
 // BRITT O'HALLORAN (britt.j.ohalloran@gmail.com)
 
 var notestack = function() {
-
-	// JQUERY NO CONFLICT
-	var $ = jQuery.noConflict();
 	
 	// ATTEMPT LOGIN
 	var sn_login = function(email,password) {
